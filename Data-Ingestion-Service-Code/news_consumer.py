1da1ce082b75beac071f7b9c14601f01a45833e2
KVMonika <korrapatimonika@gmail.com>
2021-10-10 21:18:01 +0530

    producer code segregation and consumer changes

--- Data-Ingestion-Service-Code/news_consumer.py
+++ Data-Ingestion-Service-Code/news_consumer.py
@@ -1,53 +1,15 @@
-#!/usr/bin/python3
+from kafka import KafkaConsumer
+import pymongo
+import json
+import urllib
 
-from pyspark import SparkContext
-from pyspark.sql import SparkSession
-from pyspark.streaming import StreamingContext
-from pyspark.streaming.kafka import KafkaUtils
-# import nltk
+mongo_db_client = pymongo.MongoClient("mongodb+srv://Smriti:" + urllib.parse.quote("Welcome@2021") + "@cluster0.znksh.mongodb.net/Newsfeed?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE")
+newsFeedDataBase = mongo_db_client['Newsfeed']
+collection = newsFeedDataBase['newsfeed']
 
-# nltk.download('vader_lexicon')
-# from nltk.sentiment.vader import SentimentIntensityAnalyzer
+consumer = KafkaConsumer(bootstrap_servers='localhost:9092',auto_offset_reset='earliest')
+consumer.subscribe(['articles'])
 
-
-def handle_rdd(rdd):
-    if not rdd.isEmpty():
-        print(rdd)
-
-
-sc = SparkContext(appName="Something")
-ssc = StreamingContext(sc, 5)
-
-ss = (
-    SparkSession.builder.appName("Something")
-    .config("spark.sql.warehouse.dir", "/user/hve/warehouse")
-    .config("hive.metastore.uris", "thrift://localhost:9083")
-    .enableHiveSupport()
-    .getOrCreate()
-)
-
-ss.sparkContext.setLogLevel("WARN")
-
-ks = KafkaUtils.createDirectStream(
-    ssc, ["tweets"], {"metadata.broker.list": "localhost:9092"}
-)
-
-# sid = SentimentIntensityAnalyzer()
-
-lines = ks.map(lambda x: x[1])
-print(lines)
-transform = lines.map(
-    lambda tweet: (
-        tweet,
-        int(len(tweet.split())),
-        int(len(tweet)),
-        tweet,
-    )
-)
-
-transform.foreachRDD(handle_rdd)
-
-ssc.start()
-ssc.awaitTermination()
-
-# CREATE TABLE tweets (text STRING, words INT, length INT, text STRING) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\\|' STORED AS TEXTFILE;
+for message in consumer :
+    collection.insert_one(json.loads(message.value.decode("UTF-8")))
+    print(message.value)
---
+++ Data-Ingestion-Service-Code/news_producer.py
@@ -0,0 +1,66 @@
+import requests
+import json
+import schedule
+import time
+from news_stream import send_to_topic
+from random import randint                                                                                              
+from time import sleep
+
+# def remove_unnecessary_chars(input_str):
+#     punc = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+
+#     # Removing punctuations in string
+#     # Using loop + punctuation string
+#     for ele in input_str:
+#         if ele in punc:
+#             input_str = input_str.replace(ele, "")
+    
+#     return input_str
+
+def call_news_API(search_query):
+    # API call
+    url = "https://free-news.p.rapidapi.com/v1/search"
+    querystring = {"q": search_query, "lang": "en"}
+    headers = {
+        "x-rapidapi-host": "free-news.p.rapidapi.com",
+        "x-rapidapi-key": "0aa7ff11b8msh5ac9fdb55c0684cp1b913ajsn008671c2a111",
+    }
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    if not response or response.status_code is not 200:
+        print("Not successful API call")
+        return None
+    print(response.text)
+    return response.text
+
+
+def stream_API_response(responseText):
+    response_json = json.loads(responseText)
+    articles = response_json["articles"]
+    for article in articles:
+        print(article["title"])
+        data = {}
+        data['title'] = article['title']
+        data['date'] = article['published_date']
+        data['summary'] = article['summary']
+        data['topic'] = article['topic']
+        data['source'] = article['clean_url']
+        # Send data to kafka topic
+        send_to_topic(data)
+        sleep(randint(1,4))
+
+
+def get_data():
+    responseText = call_news_API("Elon Musk")
+    if responseText is None:
+        print("Data not found")
+    else:
+        stream_API_response(responseText)
+
+
+# After every 10mins geeks() is called.
+schedule.every(1).minutes.do(get_data)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
